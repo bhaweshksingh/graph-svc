05b42e9133c8fd6896035884e633a642bb0e286f
Bhawesh Kumar Singh <bhawdeadlydan@gmail.com>
2022-03-14 17:55:02 +0530

    Generate trendline from csv

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
-# graph service service
+# Graph service
 
+This solution consumes and analyses the CSV to return graph data in API.
+The linear trendline based on the daily count is calculated.
+
+The response to the chart data endpoint includes:
+- Chart Data Points
+- Trendline start and end
+- Trendline points
 
 ## Pre requisites
 
@@ -26,3 +33,59 @@ curl -X POST 'http://localhost:8080/chart-data' \
 --header 'Content-Type: application/json'
 ```
 
+
+Response with limited Data
+
+```shell
+{
+  "data": {
+    "DataPoints": [
+      {
+        "Day": "01 Jan 2021",
+        "VisitCount": 4
+      },
+      {
+        "Day": "02 Jan 2021",
+        "VisitCount": 4
+      },
+      {
+        "Day": "03 Jan 2021",
+        "VisitCount": 3
+      },
+      {
+        "Day": "25 Jan 2022",
+        "VisitCount": 1
+      }
+    ],
+    "Trendline": {
+      "Start": {
+        "Day": "01 Jan 2021",
+        "VisitCount": 3.0402978429640433
+      },
+      "End": {
+        "Day": "25 Jan 2022",
+        "VisitCount": 2.5790718493133755
+      }
+    },
+    "TrendlinePoints": [
+      {
+        "Day": "01 Jan 2021",
+        "VisitCount": 3.0402978429640433
+      },
+      {
+        "Day": "02 Jan 2021",
+        "VisitCount": 3.0391121720292085
+      },
+      {
+        "Day": "03 Jan 2021",
+        "VisitCount": 3.037926501094374
+      },
+      {
+        "Day": "25 Jan 2022",
+        "VisitCount": 2.5790718493133755
+      }
+    ]
+  },
+  "success": true
+}
+```
--- pkg/graphinfo/dto/graph_info.go
+++ pkg/graphinfo/dto/graph_info.go
@@ -2,23 +2,78 @@ package dto
 
 import (
 	"graph-svc/pkg/graphinfo/model"
+	"math"
 	"time"
 )
 
 type ChartData struct {
-	DataPoints []Point
-	Trendline  LinearTrendline
+	DataPoints      []Point
+	Trendline       LinearTrendline
+	TrendlinePoints []Point
 }
 
 const (
-	dayLayout = "02 Jan 2006"
+	dateLayout = "02 Jan 2006"
 )
 
-func (d *ChartData) CalculateTrendline([]model.DataPoint) {
+type TrendlinePoint struct {
+	DayNum int
+	Count  int
+}
+
+func (d *ChartData) CalculateTrendline(dataPoints []model.DataPoint) {
+
 	d.Trendline = LinearTrendline{
 		Start: Point{},
 		End:   Point{},
 	}
+
+	if len(dataPoints) == 0 {
+		return
+	}
+
+	trendlinePoints := make([]TrendlinePoint, len(dataPoints))
+
+	firstDay := dataPoints[0].Day
+	trendlinePoints[0] = TrendlinePoint{DayNum: 0, Count: dataPoints[0].VisitCount}
+
+	for i := 1; i < len(dataPoints); i++ {
+		daySinceFirst := dataPoints[i].Day.Sub(firstDay)
+		dayNum := int(daySinceFirst.Hours() / 24)
+		trendlinePoints[i] = TrendlinePoint{DayNum: dayNum, Count: dataPoints[i].VisitCount}
+	}
+
+	sumX := 0.0
+	sumY := 0.0
+	sumXSquared := 0.0
+	sumYSquared := 0.0
+	sumXY := 0.0
+
+	for _, point := range trendlinePoints {
+		sumX += float64(point.DayNum)
+		sumY += float64(point.Count)
+		sumXSquared += math.Pow(float64(point.DayNum), 2)
+		sumYSquared += math.Pow(float64(point.Count), 2)
+
+		sumXY += float64(point.DayNum * point.Count)
+	}
+
+	b := (float64(len(trendlinePoints))*sumXY - sumX*sumY) / (float64(len(trendlinePoints))*sumXSquared - math.Pow(sumX, 2))
+
+	a := sumY/float64(len(trendlinePoints)) - sumX*b/float64(len(trendlinePoints))
+
+	trendlineCalculated := make([]Point, len(dataPoints))
+
+	for i, point := range trendlinePoints {
+		visitCountTrend := a + b*float64(point.DayNum)
+		trendlineCalculated[i] = Point{Day: dataPoints[i].Day.Format(dateLayout), VisitCount: visitCountTrend}
+	}
+
+	d.Trendline.Start = trendlineCalculated[0]
+	d.Trendline.End = trendlineCalculated[len(trendlineCalculated)-1]
+
+	d.TrendlinePoints = trendlineCalculated
+
 }
 
 type LinearTrendline struct {
@@ -28,13 +83,13 @@ type LinearTrendline struct {
 
 type Point struct {
 	Day        string
-	VisitCount int
+	VisitCount float64
 }
 
 func NewChartDataResponse(dataPoints []model.DataPoint) ChartData {
 	chartData := &ChartData{}
 	for _, point := range dataPoints {
-		chartData.DataPoints = append(chartData.DataPoints, Point{getDateString(point.Day), point.VisitCount})
+		chartData.DataPoints = append(chartData.DataPoints, Point{getDateString(point.Day), float64(point.VisitCount)})
 	}
 
 	chartData.CalculateTrendline(dataPoints)
@@ -43,5 +98,5 @@ func NewChartDataResponse(dataPoints []model.DataPoint) ChartData {
 }
 
 func getDateString(roundedTime time.Time) string {
-	return roundedTime.Format(dayLayout)
+	return roundedTime.Format(dateLayout)
 }
